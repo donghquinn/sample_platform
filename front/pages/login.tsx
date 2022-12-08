3c3a83e0845aa004e5691353a1559bfe3e8d1df1
Dong H. Kim <ehdgus1524@gmail.com>
2022-12-08 14:42:45 +0900

    [CHANGED] recoil Added

--- front/pages/login.tsx
+++ front/pages/login.tsx
@@ -8,6 +8,10 @@ function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  // 패스워드, 이메일 입력
+  const onEmailChange = (e) => setEmail(e.target.value);
+  const onPasswordChange = (e) => setPassword(e.target.value);
+
   return (
     <>
       <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
@@ -33,6 +37,7 @@ function Login() {
                   required
                   className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                   placeholder="Email address"
+                  onChange={onEmailChange}
                 />
               </div>
               <div>
@@ -47,6 +52,7 @@ function Login() {
                   required
                   className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                   placeholder="Password"
+                  onChange={onPasswordChange}
                 />
               </div>
             </div>
@@ -90,7 +96,6 @@ function Login() {
               <button
                 type="submit"
                 className="group relative flex w-full justify-center rounded-md border border-transparent py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                onClick={}
               >
                 <span className="absolute inset-y-0 left-0 flex items-center pl-3">
                   <LockClosedIcon
---
+++ front/src/libraries/recoil.lib.ts
@@ -0,0 +1,11 @@
+import { atom } from "recoil";
+
+export const emailManage = atom({
+  key: "emailManage",
+  default: "",
+});
+
+export const passwordManage = atom({
+  key: "passwordManage",
+  default: "",
+});
