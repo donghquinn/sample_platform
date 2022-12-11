c1ecdba545a39dd1ceddcb8b8879b3f44184a7e0
Dong H. Kim <ehdgus1524@gmail.com>
2022-12-11 23:02:09 +0900

    [CHANGED] Logging createToken

--- back/src/libraries/createToken.lib.ts
+++ back/src/libraries/createToken.lib.ts
@@ -1,5 +1,5 @@
 import { createHash } from 'crypto';
-import { stringify } from 'querystring';
+import { Logger } from 'utils';
 import { v4 } from 'uuid';
 
 // jwt 생성
@@ -11,13 +11,19 @@ export const createToken = () => {
 
   // const { clientid: clientKey } = await Mysql.query<ClientInfo>(selectAdminId, [email, password]);
 
+  Logger.info('[REGISTER] keys: %o', { secretKey, clientKey });
+
   const rawKeys = `${clientKey}${secretKey}`;
 
-  const encoded = stringify({ rawKeys: Number(rawKeys) });
+  Logger.info('[REGISTER] rawKeys: %o', rawKeys);
+
+  // const encoded = stringify({ rawKeys: Number(rawKeys) });
 
   const hashBase = createHash('sha256');
-  const hash = hashBase.update(encoded, 'utf-8').digest('hex');
+  const hash = hashBase.update(rawKeys, 'utf-8').digest('hex');
   const uuid = v4();
+
+  Logger.info('[REGISTER] hash etc: %o', { hash, uuid, clientKey, secretKey });
 
   return { hash, uuid, clientKey, secretKey };
 };
