d5c58f3e01b303d601e48f2265a3740882700912
Dong H. Kim <ehdgus1524@gmail.com>
2022-12-13 12:08:12 +0900

    [CHANGEd] logout btn component

--- front/component/logout-btn.tsx
+++ front/component/logout-btn.tsx
@@ -1,13 +1,38 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useRecoilState } from "recoil";
+import {
+  clientidManage,
+  loginManage,
+  tokenManage,
+} from "../src/libraries/recoil.lib";
 
 function LogoutBtn() {
+  const router = useRouter();
+
+  const [token, setToken] = useRecoilState(tokenManage);
+  const [login, setLogin] = useRecoilState(loginManage);
+  const [clientid, setClientid] = useRecoilState(clientidManage);
+
   return (
     <div>
-      <div className="flex flex-col content-center">
+      <div className="flex flex-col content-center" style={{ margin: 30 }}>
         <div className="flex justify-center">
-          <Link href="/logout">
-            <button className="btn">로그아웃하기</button>
-          </Link>
+          <button
+            className="btn"
+            onClick={() => {
+              setToken("");
+              setClientid("");
+
+              setLogin(false);
+
+              alert("로그아웃 되었습니다.");
+
+              router.push("/");
+            }}
+          >
+            로그아웃하기
+          </button>
         </div>
       </div>
     </div>
